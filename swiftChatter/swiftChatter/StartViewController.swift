9a57cc4c3967f6b27449a6f8f5bea98ee9463111
cmcohn22 <47287135+cmcohn22@users.noreply.github.com>
2021-11-01 11:50:55 -0400

    added sign in pages

---
+++ swiftChatter/swiftChatter/StartViewController.swift
@@ -0,0 +1,10 @@
+//
+//  StartViewController.swift
+//  swiftChatter
+//
+//  Created by Chase Cohn on 11/1/21.
+//  Copyright © 2021 The Regents of the University of Michigan. All rights reserved.
+//
+
+import Foundation
+
