8cc995be475030161d69d4f4ada8712117924f21
cmcohn22 <47287135+cmcohn22@users.noreply.github.com>
2021-11-01 12:08:08 -0400

    more progress

---
+++ swiftChatter/swiftChatter/LogInViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LogInViewController.swift
+//  swiftChatter
+//
+//  Created by Chase Cohn on 11/1/21.
+//  Copyright © 2021 The Regents of the University of Michigan. All rights reserved.
+//
+
+import Foundation
---
+++ swiftChatter/swiftChatter/SignUpViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpViewController.swift
+//  swiftChatter
+//
+//  Created by Chase Cohn on 11/1/21.
+//  Copyright © 2021 The Regents of the University of Michigan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class SignUpViewController {
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var passwordConfirm: UIButton!
+}
