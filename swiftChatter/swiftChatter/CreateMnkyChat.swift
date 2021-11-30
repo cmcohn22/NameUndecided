18c050ca95d4e6f512aa4c7dd2e5f2a197742b7e
Griffin Kaufman <griffkau@umich.edu>
2021-11-30 11:29:19 -0500

    Update from Tuesday Morning

---
+++ swiftChatter/swiftChatter/CreateMnkyChat.swift
@@ -0,0 +1,10 @@
+//
+//  CreateMnkyChat.swift
+//  swiftChatter
+//
+//  Created by Griffin Kaufman on 11/30/21.
+//  Copyright © 2021 The Regents of the University of Michigan. All rights reserved.
+//
+
+import Foundation
+
