8525a8160631b16b62d08fd6e8be9311b257b688
Griffin Kaufman <griffkau@umich.edu>
2021-12-02 21:16:06 -0500

    Added Message Files

---
+++ swiftChatter/swiftChatter/MessageTableCell.swift
@@ -0,0 +1,17 @@
+//
+//  MessageTableCell.swift
+//  swiftChatter
+//
+//  Created by Griffin Kaufman on 12/2/21.
+//  Copyright © 2021 The Regents of the University of Michigan. All rights reserved.
+//
+
+import UIKit
+
+final class MessageTableCell: UITableViewCell {
+  
+    @IBOutlet weak var firstnameLabel: UILabel! // TODO: disconnect before connecting
+    @IBOutlet weak var lastnameLabel: UILabel! // TODO: disconnect before connecting
+    @IBOutlet weak var contentLabel: UILabel! // TODO: disconnect before connecting
+//    @IBOutlet weak var imageLabel: UIImageView! // If we choose to add prof_picture
+}
