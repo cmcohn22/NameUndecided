f9c0d03f9b95f46171ff39221762c84f10d48a98
Griffin Kaufman <griffkau@umich.edu>
2021-12-03 02:04:52 -0500

    combined code

--- swiftChatter/swiftChatter/AppDelegate.swift
+++ swiftChatter/swiftChatter/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 import Firebase
 @UIApplicationMain
 
-2class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
