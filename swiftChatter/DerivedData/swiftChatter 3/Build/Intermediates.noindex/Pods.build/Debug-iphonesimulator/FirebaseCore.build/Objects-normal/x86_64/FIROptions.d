566770356640e6915a528a6f7d85161572ee219c
cmcohn22 <47287135+cmcohn22@users.noreply.github.com>
2021-12-07 15:55:04 -0500

    For griff

---
+++ swiftChatter/DerivedData/swiftChatter 3/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/FirebaseCore.build/Objects-normal/x86_64/FIROptions.d
@@ -0,0 +1,26 @@
+dependencies: \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/FIROptions.m \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/FirebaseCore.build/module.modulemap \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/FIRBundleUtil.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/kcdata.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/uuid.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/netinet6.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/machine.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/device.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/mach_debug.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/mach_machine.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/mach.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/bank.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/darwin_posix_sys_types.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/netinet.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/net.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/darwin_cdefs.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/mach-o/dyld.modulemap \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/Private/FIRAppInternal.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/Public/FirebaseCore/FIRApp.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/Private/FIRLogger.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/Public/FirebaseCore/FIRLoggerLevel.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/Private/FIROptionsInternal.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/Public/FirebaseCore/FIROptions.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/Public/FirebaseCore/FIRVersion.h
---
+++ swiftChatter/DerivedData/swiftChatter 3/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/FirebaseInstallations.build/Objects-normal/x86_64/FIRInstallationsIDController.d
@@ -0,0 +1,100 @@
+dependencies: \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/InstallationsIDController/FIRInstallationsIDController.m \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/FirebaseInstallations.build/module.modulemap \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/InstallationsIDController/FIRInstallationsIDController.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/kcdata.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/uuid.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/netinet6.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/machine.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/device.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/mach_debug.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/mach_machine.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/mach.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/bank.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/darwin_posix_sys_types.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/netinet.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/net.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/darwin_cdefs.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator15.0.sdk/usr/include/mach-o/dyld.modulemap \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromises.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/PromisesObjC.build/module.modulemap \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Wrap.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Validate.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Timeout.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Then.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Testing.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Retry.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Reduce.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Recover.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Race.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Do.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Delay.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Catch.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Await.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Async.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Any.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+Always.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromiseError.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Products/Debug-iphonesimulator/PromisesObjC/FBLPromises.framework/Headers/FBLPromise+All.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/Target\ Support\ Files/PromisesObjC/PromisesObjC-umbrella.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/GoogleUtilities.build/module.modulemap \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/UserDefaults/Public/GoogleUtilities/GULUserDefaults.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Reachability/Public/GoogleUtilities/GULReachabilityChecker.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Network/Public/GoogleUtilities/GULNetworkURLSession.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Network/Public/GoogleUtilities/GULNetworkMessageCode.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Network/Public/GoogleUtilities/GULNetworkLoggerProtocol.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Network/Public/GoogleUtilities/GULNetworkConstants.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Network/Public/GoogleUtilities/GULNetwork.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Network/Public/GoogleUtilities/GULMutableDictionary.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/NSData+zlib/Public/GoogleUtilities/GULNSData+zlib.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/MethodSwizzler/Public/GoogleUtilities/GULSwizzler.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/MethodSwizzler/Public/GoogleUtilities/GULOriginalIMPConvenienceMacros.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Logger/Public/GoogleUtilities/GULLoggerLevel.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Logger/Public/GoogleUtilities/GULLogger.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Environment/Public/GoogleUtilities/NSURLSession+GULPromises.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Environment/Public/GoogleUtilities/GULURLSessionDataResponse.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Environment/Public/GoogleUtilities/GULSecureCoding.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Environment/Public/GoogleUtilities/GULKeychainUtils.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Environment/Public/GoogleUtilities/GULKeychainStorage.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Environment/Public/GoogleUtilities/GULHeartbeatDateStorageUserDefaults.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Environment/Public/GoogleUtilities/GULHeartbeatDateStorage.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Environment/Public/GoogleUtilities/GULHeartbeatDateStorable.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/Environment/Public/GoogleUtilities/GULAppEnvironmentUtil.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/AppDelegateSwizzler/Public/GoogleUtilities/GULSceneDelegateSwizzler.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/AppDelegateSwizzler/Public/GoogleUtilities/GULApplication.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/GoogleUtilities/GoogleUtilities/AppDelegateSwizzler/Public/GoogleUtilities/GULAppDelegateSwizzler.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/Target\ Support\ Files/GoogleUtilities/GoogleUtilities-umbrella.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseCore/Sources/Private/FirebaseCoreInternal.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/DerivedData/swiftChatter\ 3/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/FirebaseCore.build/module.modulemap \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/Public/FirebaseCore/FIRVersion.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/Public/FirebaseCore/FIROptions.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/Public/FirebaseCore/FirebaseCore.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/Public/FirebaseCore/FIRLoggerLevel.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/Public/FirebaseCore/FIRConfiguration.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseCore/FirebaseCore/Sources/Public/FirebaseCore/FIRApp.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/Target\ Support\ Files/FirebaseCore/FirebaseCore-umbrella.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseCore/Sources/Private/FIRAppInternal.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseCore/Sources/Private/FIRComponent.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseCore/Sources/Private/FIRComponentContainer.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseCore/Sources/Private/FIRComponentType.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseCore/Sources/Private/FIRDependency.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseCore/Sources/Private/FIRHeartbeatInfo.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseCore/Sources/Private/FIRLibrary.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseCore/Sources/Private/FIRLogger.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseCore/Sources/Private/FIROptionsInternal.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/Errors/FIRInstallationsErrorUtil.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/Public/FirebaseInstallations/FIRInstallationsErrors.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/FIRInstallationsItem.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/InstallationsIDController/FIRInstallationsStatus.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/FIRInstallationsLogger.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/IIDMigration/FIRInstallationsIIDStore.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/IIDMigration/FIRInstallationsIIDTokenStore.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/InstallationsAPI/FIRInstallationsAPIService.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/InstallationsIDController/FIRInstallationsBackoffController.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/InstallationsIDController/FIRCurrentDateProvider.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/InstallationsIDController/FIRInstallationsSingleOperationPromiseCache.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/InstallationsStore/FIRInstallationsStore.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/Errors/FIRInstallationsHTTPError.h \
+  /Users/chasecohn/Desktop/MNKY\ chat/NameUndecided/swiftChatter/Pods/FirebaseInstallations/FirebaseInstallations/Source/Library/InstallationsStore/FIRInstallationsStoredAuthToken.h
