3b3e1650f36eedc0f4509c011d748188a85f3e8e
Griffin Kaufman <griffkau@umich.edu>
2021-12-07 23:15:20 -0500

    pratik merge finished

--- swiftChatter/swiftChatter/AppDelegate.swift
+++ swiftChatter/swiftChatter/AppDelegate.swift
@@ -5,23 +5,19 @@
 //  Created by sugih on 7/23/20.
 //  Copyright © 2020 The Regents of the University of Michigan. All rights reserved.
 //
-
 import UIKit
 
-import Firebase
 @UIApplicationMain
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -36,4 +32,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
